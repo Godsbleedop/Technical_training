7892b0ba3f03109d43c21a96a92cc781e48911a7
Aaron Quadras <121336532+Godsbleedop@users.noreply.github.com>
2025-02-10 11:04:11 +0530

    Update RandomNum.java

--- RandomNum.java
+++ RandomNum.java
@@ -1,2 +1,18 @@
 //customer support ticket id generation
 // a customer suport system generates ticket id   in the format of TKT-<customer_name>-<random_number> the random number should be 1000-9999
+import java.util.Random;
+import java.util.Scanner;
+
+class Rand{
+public static void main(String [] args){
+Scanner scanner = new Scanner(System.in);
+Random random = new Random();
+
+  int randomInt = random.nextInt(9999);
+ System.out.println(" enter your name ");
+ String name = scanner.nextLine();
+
+ System.out.println(" your ticket is TKT-"+name+"-"+randomInt);
+        
+}
+}
