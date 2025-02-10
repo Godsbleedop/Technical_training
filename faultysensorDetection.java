6bfe56a130a35c34e56421035906c253880c690a
Aaron Quadras <121336532+Godsbleedop@users.noreply.github.com>
2025-02-10 09:17:05 +0530

    Update faultysensorDetection.java

--- faultysensorDetection.java
+++ faultysensorDetection.java
@@ -1,1 +1,2 @@
-
+// a machine has 10 sensors that give temperature readings .  the normal 20-40 degree celcius , but if any sensor gives a readding above 50 degree celcius there might a fault 
+// 25,30,28,55,32,29
