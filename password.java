e27acef726cd849ab311f7aa68962f0de36492c0
Aaron Quadras <121336532+Godsbleedop@users.noreply.github.com>
2025-02-10 13:42:47 +0530

    Update password.java

--- password.java
+++ password.java
@@ -1,4 +1,12 @@
 // password length more than 5 less than 15
 // the characters of a-z and A-Z      
 
+import java.util.Scanner;
+class palindrome{
+public static void main(String[] args){
 
+Scanner sc = new Scanner(System.in);
+System.out.println(" enter the string ");
+String name = sc.nextLine();
+StringBuffer stringbuff = new StringBuffer(name);
+String rev =stringbuff.toString();
