107ba10dba022d5b90c06df8c2c1bfd0926b2f5f
Aaron Quadras <121336532+Godsbleedop@users.noreply.github.com>
2025-02-10 09:34:47 +0530

    Update faultysensorDetection.java
    this code is absolute garbage and is bloated but i have to learn somehow so this is why i made this code

--- faultysensorDetection.java
+++ faultysensorDetection.java
@@ -1,2 +1,32 @@
 // a machine has 10 sensors that give temperature readings .  the normal 20-40 degree celcius , but if any sensor gives a readding above 50 degree celcius there might a fault 
 // 25,30,28,55,32,29
+import java.util.Scanner;
+
+class Fault{
+public static void main(String[] args){
+ Scanner sc = new Scanner(System.in);
+ int arr[] = new int[6];
+System.out.println(" enter the array");
+ for(int i=0;i<6;i++){
+  
+  arr[i] = sc.nextInt();
+ 
+
+ }
+ for(int i=0;i<6;i++){
+  System.out.println(" the array is "+arr[i]);
+ }
+ System.out.println(" to check if the temp reading is correct ");
+ 
+ for( int i=0;i<6;i++){
+   if(arr[i]>50){
+   System.out.println(" the value of the sensor is incorrect "+i);
+   
+   }
+   else{
+   System.out.println(" the sensor values are correct at  "+i);
+   }
+ sc.close();
+ }
+}
+   }
