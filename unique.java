f6e4ebe9ddeaca5ee5987bdd5202516e7d9f44c4
Aaron Quadras <121336532+Godsbleedop@users.noreply.github.com>
2025-02-11 10:27:13 +0530

    Create unique.java

---
+++ unique.java
@@ -0,0 +1,3 @@
+// a customer support system log mulitple issues reported by the users the first unqiue characater word  in a log is often the most critical issue given a message 
+// find the first non repeating character if all the character repeats return -1 
+//else return unqiue character 
