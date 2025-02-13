c884c9ee6f9a69e85fc89c6ae7bda93a97a792a6
Aaron Quadras <121336532+Godsbleedop@users.noreply.github.com>
2025-02-13 13:54:22 +0530

    Create undo_redo.java

---
+++ undo_redo.java
@@ -0,0 +1,9 @@
+//undo ||  redo feature in a text editor 
+//  break points 
+/*  intialize two stacks undo stack and redo stack 
+2) perform an action ( typing /deleting ) push action onto undo stack clear redo stack  
+3)) STEPP 3 undo stack is nnot empty poop the  and push it onto  redo stack 
+apply the undone state 
+step 4) *1 if redo stack is not empty pop the last undone action and push it onto undo stack 
+*2  apply the redone state 
+  */
