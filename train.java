199948f5fd79483b6068304187f368e79a594670
Aaron Quadras <121336532+Godsbleedop@users.noreply.github.com>
2025-02-14 10:30:35 +0530

    Create train.java

---
+++ train.java
@@ -0,0 +1,5 @@
+// at a railway ticket counter people stand in a queue  to buy a counter 
+// the first person in a queue to buy a ticket 
+// a person joined a queue push the name. The first person joined the queue leaves
+// check the next person to be served 
+// display the total mnumber of people in the queue 
