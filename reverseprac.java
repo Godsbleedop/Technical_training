0868c2dba3ec7da0bfef3fdece0b63ab06e84f48
Aaron Quadras <121336532+Godsbleedop@users.noreply.github.com>
2025-02-14 09:26:35 +0530

    Update reverseprac.java

--- reverseprac.java
+++ reverseprac.java
@@ -1,2 +1,9 @@
 /* write a programm to reverse a string 
 */
+import java.util.Stack ;
+import java.util.Scanner;
+ class reverse {
+   public static void main(String[] args ){
+      System.out.println(" enter the stack ");
+     Scanner sc = new Scanner(System.in);
+     
